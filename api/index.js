617baff885229bc1ecb65ed499bfde4fbbe70741
fmas1 <famascba@gmail.com>
2026-06-10 23:29:54 -0300

    fix: Vercel native Express support, remove serverless-http

--- api/index.js
+++ api/index.js
@@ -1,5 +1,2 @@
-const serverless = require('serverless-http');
 const createApp = require('../app');
-
-const app = createApp();
-module.exports.handler = serverless(app);
+module.exports = createApp();
